8e33cabe3e42cabc8dba7f822f03c35b87fce02f
Rafael Chacón <rafael.ichacon@gmail.com>
2024-01-10 14:56:02 -0400

    Update to 2.0.45 version

--- lib/layrz_models.freezed.dart
+++ lib/layrz_models.freezed.dart
@@ -60729,7 +60729,7 @@ class _$SimulationCycleImpl implements _SimulationCycle {
   }
 
   @override
-  bool operator ==(dynamic other) {
+  bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _$SimulationCycleImpl &&
