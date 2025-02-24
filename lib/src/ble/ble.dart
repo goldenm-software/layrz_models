f5228c95de0734cb8630c04f72cc8d6a9847c0b7
Kenny Mochizuki <kenny@goldenm.com>
2025-02-24 16:17:04 -0600

    Lint

--- lib/src/ble/ble.dart
+++ lib/src/ble/ble.dart
@@ -1,10 +1,7 @@
 library ble;
 
-import 'dart:typed_data';
-
 import 'package:collection/collection.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:layrz_models/src/converters/converters.dart';
 
 part 'ble.freezed.dart';
 part 'ble.g.dart';
