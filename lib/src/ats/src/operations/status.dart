d29d4104ab6e29036a247a775f29d5b4094ccf59
rchacon <rafael.ichacon@gmail.com>
2025-03-31 22:56:07 -0400

    Update AtsPurchaseOrderStatus enum and AtsOperationStatuses model

--- lib/src/ats/src/operations/status.dart
+++ lib/src/ats/src/operations/status.dart
@@ -15,6 +15,12 @@ class AtsOperationStatuses with _$AtsOperationStatuses {
     /// `deliveredAt` is the Unix timestamp of when the asset was delivered.
     @TimestampOrNullConverter() DateTime? deliveredAt,
 
+    /// `unloadingOperationAt` is the Unix timestamp of when the asset was unloading operation.
+    @TimestampOrNullConverter() DateTime? unloadingOperationAt,
+
+    /// `readyToOperateAt` is the Unix timestamp of when the asset was ready to operate.
+    @TimestampOrNullConverter() DateTime? readyToOperateAt,
+
     /// `eta` is the estimated time of arrival in Unix timestamp.
     @TimestampOrNullConverter() DateTime? eta,
   }) = _AtsOperationStatuses;
